b8e5eb8e5143ab18f3781b4beb6ad7cec7542be7
MFirdavs <mustafoqulovfirdavs13@gmail.com>
2023-10-02 13:20:27 +0500

    Updates

--- src/main/java/com/example/geoservice/controller/GeoController.java
+++ src/main/java/com/example/geoservice/controller/GeoController.java
@@ -1,20 +1,14 @@
 package com.example.geoservice.controller;
 
-import com.example.geoservice.entity.Geo;
 import com.example.geoservice.service.GeoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.wololo.geojson.Feature;
 import org.wololo.geojson.FeatureCollection;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,11 +16,6 @@ import java.util.Optional;
 public class GeoController {
 
     private final GeoService geoService;
-
-//    @GetMapping("all")
-//    public FeatureCollection getAllGeo(){
-//        return geoService.getAll();
-//    }
 
     @GetMapping("all")
     public ResponseEntity<FeatureCollection> getAll(){
